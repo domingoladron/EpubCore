3a1d5b39ebbc51729d93ef8c1560e6d04fb52196
Chris Laine <christopher.ian.laine@gmail.com>
2023-04-23 17:28:39 +1200

    Adding a fluent builder class

---
+++ Penman.EpubSharp/Fluent/EpubBookBuilder.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+using Penman.EpubSharp.Format;
+
+namespace Penman.EpubSharp.Fluent;
+
+public class EpubBookBuilder : IEpubBookBuilder
+{
+    private readonly IEpubWriter _writer;
+
+    public EpubBookBuilder(IEpubWriter writer)
+    {
+        _writer = writer;
+    }
+    public IEpubBookBuilder WithTitle(string title)
+    {
+       _writer.SetTitle(title);
+       return this;
+    }
+
+    public IEpubBookBuilder AddAuthor(string authorName)
+    {
+        _writer.AddAuthor(authorName);
+        return this;
+    }
+
+    public IEpubBookBuilder AddChapter(string title, string chapterContents)
+    {
+        _writer.AddChapter(title, chapterContents);
+        return this;
+    }
+
+    public IEpubBookBuilder AddStylesheet(string stylesheetName, string stylesheetContents)
+    {
+        return AddFileContent(stylesheetName, stylesheetContents, EpubContentType.Css);
+    }
+
+    public IEpubBookBuilder AddStylesheet(string stylesheetName, byte[] stylesheetContents)
+    {
+        return AddFileContent(stylesheetName, stylesheetContents, EpubContentType.Css);
+    }
+
+    public IEpubBookBuilder AddJpg(string imageName, byte[] imageContents)
+    {
+        return AddFileContent(imageName, imageContents, EpubContentType.ImageJpeg);
+    }
+
+    public IEpubBookBuilder AddGif(string imageName, byte[] imageContents)
+    {
+        return AddFileContent(imageName, imageContents, EpubContentType.ImageGif);
+    }
+
+    public IEpubBookBuilder AddPng(string imageName, byte[] imageContents)
+    {
+        return AddFileContent(imageName, imageContents, EpubContentType.ImagePng);
+    }
+
+    public IEpubBookBuilder AddSvg(string imageName, byte[] imageContents)
+    {
+        return AddFileContent(imageName, imageContents, EpubContentType.ImageSvg);
+    }
+
+    public IEpubBookBuilder AddTrueTypeFont(string fontFileName, byte[] fontContents)
+    {
+        return AddFileContent(fontFileName, fontContents, EpubContentType.FontTruetype);
+    }
+
+    public IEpubBookBuilder AddOpenTypeFont(string fontFileName, byte[] fontContents)
+    {
+        return AddFileContent(fontFileName, fontContents, EpubContentType.FontOpentype);
+    }
+
+    public IEpubBookBuilder AddFileContent(string fileName, string fileContents, EpubContentType type)
+    {
+        _writer.AddFile(fileName, fileContents, type);
+        return this;
+    }
+
+    public IEpubBookBuilder AddFileContent(string fileName, byte[] fileContents, EpubContentType type)
+    {
+        _writer.AddFile(fileName, fileContents, type);
+        return this;
+    }
+
+    public void Build(string savePath)
+    {
+        _writer.Write(savePath);
+    }
+
+    public void Build(Stream stream)
+    {
+        _writer.Write(stream);
+    }
+
+    public static EpubBookBuilder Create()
+    {
+        return new EpubBookBuilder(new EpubWriter());
+    }
+}
---
+++ Penman.EpubSharp/Fluent/IEpubBookBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Penman.EpubSharp.Format;
+
+namespace Penman.EpubSharp.Fluent
+{
+    /// <summary>
+    /// A Builder for fluently constructing an Epub Book 
+    /// </summary>
+    public interface IEpubBookBuilder
+    {
+        IEpubBookBuilder WithTitle(string title);
+        IEpubBookBuilder AddAuthor(string authorName);
+        IEpubBookBuilder AddChapter(string title, string chapterContents);
+
+        IEpubBookBuilder AddStylesheet(string stylesheetName, string stylesheetContents);
+
+        IEpubBookBuilder AddStylesheet(string stylesheetName, byte[] stylesheetContents);
+
+        IEpubBookBuilder AddJpg(string imageName, byte[] imageContents);
+
+        IEpubBookBuilder AddGif(string imageName, byte[] imageContents);
+
+        IEpubBookBuilder AddPng(string imageName, byte[] imageContents);
+
+        IEpubBookBuilder AddSvg(string imageName, byte[] imageContents);
+
+        IEpubBookBuilder AddTrueTypeFont(string fontFileName, byte[] fontContents);
+
+        IEpubBookBuilder AddOpenTypeFont(string fontFileName, byte[] fontContents);
+
+        IEpubBookBuilder AddFileContent(string fileName, string fileContents, EpubContentType type);
+
+        IEpubBookBuilder AddFileContent(string fileName, byte[] fileContents, EpubContentType type);
+
+        void Build(string savePath);
+
+        void Build(Stream stream);
+    }
+}
