92ed119eb4fe874103772c8c7112bf28f12bc076
Chris Laine <christopher.ian.laine@gmail.com>
2023-07-10 08:43:16 +1200

    Tidying up example app

--- Penman.EpubSharp.ExampleApp/Program.cs
+++ Penman.EpubSharp.ExampleApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Penman.EpubSharp;
 
+
+// An example of reading from an epub.
+
 var pathToEPubToRead = $"{AppContext.BaseDirectory}/siddhartha-hermanhesse.epub";
 
 var book = EpubReader.Read(pathToEPubToRead);
@@ -8,26 +11,27 @@ var uniqueIdentifier = book.UniqueIdentifier;
 var title = book.Title;
 var authors = book.Authors;
 var publishers = book.Publishers;
-
-var cover = book.CoverImage;
 var coverImageName = book.CoverImageHref;
 
 // Get table of contents
 var chapters = book.TableOfContents;
 
 // Get contained files
-var htmlFiles = book.Resources.Html;
 var css = book.Resources.Css;
-var images = book.Resources.Images;
+var cssFileNames = css.Select(cssFile => cssFile.FileName).ToList();
 var fonts = book.Resources.Fonts;
 
 
 
-Console.WriteLine($"Book Title: {title}");
+Console.WriteLine($"Book Title: {title.Trim()}");
+Console.WriteLine($"Book Unique Identifier: {uniqueIdentifier}");
 Console.WriteLine($"Book Authors: {string.Join(",", authors)}");
 Console.WriteLine($"Book Publishers: {string.Join(",", publishers)}");
+Console.WriteLine($"Book Cover file name: {coverImageName}");
+Console.WriteLine($"Book Css Files: {string.Join(",", cssFileNames)}");
+Console.WriteLine($"Book Fonts: {string.Join(",", fonts)}");
+Console.WriteLine("Chapters");
 foreach (var curChapter in chapters)
 {
     Console.WriteLine($"Chapter Title: {curChapter.Title}");
-    Console.WriteLine( $"Chapter Content Type: {curChapter.LinkedResource?.ContentType}");
 }
