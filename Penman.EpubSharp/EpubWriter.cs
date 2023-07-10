a9a3342959d872047ea517e42536c5d03c1e85ee
Chris Laine <christopher.ian.laine@gmail.com>
2023-07-10 20:02:26 +1200

    Fixed broken title test

--- Penman.EpubSharp/EpubWriter.cs
+++ Penman.EpubSharp/EpubWriter.cs
@@ -190,6 +190,7 @@ namespace Penman.EpubSharp
         public void SetTitle(string title)
         {
             if (string.IsNullOrWhiteSpace(title)) throw new ArgumentNullException(nameof(title));
+            RemoveTitle();
             _format.Opf.Metadata.Titles.Add(title);
         }
 
