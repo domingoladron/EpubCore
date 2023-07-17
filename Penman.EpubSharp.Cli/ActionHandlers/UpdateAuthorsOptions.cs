2a398422c203773f200b525d5e222e23539fd947
Chris Laine <christopher.ian.laine@gmail.com>
2023-07-17 20:41:55 +1200

    #3: Adding update authors tests

--- Penman.EpubSharp.Cli/ActionHandlers/UpdateAuthorsOptions.cs
+++ Penman.EpubSharp.Cli/ActionHandlers/UpdateAuthorsOptions.cs
@@ -5,10 +5,10 @@ namespace Penman.EpubSharp.Cli.ActionHandlers;
 [Verb("update-authors", HelpText = "Update the author(s) in the epub.")]
 public class UpdateAuthorsOptions : EpubManipulatorOptionsBase
 {
-    [Option('a', "authors", Required = true, HelpText = "Authors to swap out of our EPub", Min=1)]
+    [Option('a', "author", Required = true, HelpText = "Author to swap out of our EPub", Min=1)]
     public IEnumerable<string> Authors { get; set; }
 
-    [Option('c', "clear-previous", HelpText = "Clears previous authors")]
+    [Option('c', "clear-previous", HelpText = "Clear previous authors", Default = false)]
     public bool ClearPrevious { get; set; }
 
 }
