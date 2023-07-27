4530722a4b303796c5b70ea5eb3adf0b8c6442f9
Chris Laine <christopher.ian.laine@gmail.com>
2023-07-28 08:23:42 +1200

    #28: Reworking option params to be easier

--- EpubCore.Cli/ActionHandlers/EpubManipulatorOptionsBase.cs
+++ EpubCore.Cli/ActionHandlers/EpubManipulatorOptionsBase.cs
@@ -4,6 +4,6 @@ namespace EpubCore.Cli.ActionHandlers;
 
 public class EpubManipulatorOptionsBase : EpubInputOptionsBase
 {
-    [Option('o', "output-epub", Required = false, HelpText = "Path to write final epub.  If empty, overwrites input epub")]
+    [Option('o', "out", Required = false, HelpText = "Path to write final epub.  If empty, overwrites input epub")]
     public string? OutputEpub { get; set; }
 }
